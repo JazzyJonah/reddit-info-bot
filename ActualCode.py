3e7134ab3175a2594274833585f32e1c58a91f78
JazzyJonah <96821467+JazzyJonah@users.noreply.github.com>
2022-09-21 21:46:54 -0400

    Actually added code
    Printed something when it wakes up, made a command for it.

--- ActualCode.py
+++ ActualCode.py
@@ -1,2 +1,21 @@
 #this is a test
-clientSecret = secrets.discordOathKey
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="r!")
+client.remove_command("help")
+
+
+@client.event
+async def on_ready():
+	print(f"We have logged in as {client.user}")
+	await client.change_precense(activity = discord.Activity(
+		type = discord.ActivityType.watching,
+		name = "Reddit posts." )
+	)
+
+@client.command(pass_context = True)
+async def reddit():
+	await ctx.send("reddit.com")
+
+client.run(str(secrets.discordOathKey))
