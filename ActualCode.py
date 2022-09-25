4a07dca9d0e22134988fa5195e47785ca451ee03
JazzyJonah <96821467+JazzyJonah@users.noreply.github.com>
2022-09-24 23:35:53 -0400

    Update ActualCode.py

--- ActualCode.py
+++ ActualCode.py
@@ -1,25 +1,18 @@
-#this is a test
 import discord
 import os
-from discord.ext import commands
-from discord import Intents
-from dotenv import load_dotenv
 
-intents = Intents.default()
-intents.members = True
+intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix="r!", intents = intents)
-client.remove_command("help")
 
-load_dotenv()
-
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-	print(f"We have logged in as {client.user}")
+    print(f'We have logged in as {client.user}')
 
+@client.event
 async def on_message(message):
-	if message.startswith("r!reddit"):
-		await message.channel.send("reddit.com")
+    if message.content.startswith('r!reddit'):
+        await message.channel.send('reddit.com')
 
-client.run(os.getenv("DISCORD_TOKEN"))
+client.run(os.getenv("DISCORD_TOKEN"))  
